0b0f8304dc0e2ae4b514eda2167ee018b22c7990
acuD1 <arsciand@student.42.fr>
2022-10-12 12:50:27 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-TAG=$TAG caprover deploy --appName $INPUT_APP --appToken $INPUT_TOKEN --caproverUrl $INPUT_SERVER --tarFile ./deploy.tar
+TAG=$TAG caprover deploy --appName $INPUT_APP --appToken $INPUT_TOKEN --caproverUrl $INPUT_SERVER -b pre-prod
