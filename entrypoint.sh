f8672481df6ea7a3a71c200239793158962ac650
acuD1 <arsciand@student.42.fr>
2022-10-12 12:06:39 +0200

    Test TAG

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-caprover deploy --appName $INPUT_APP --appToken $INPUT_TOKEN --caproverUrl $INPUT_SERVER --tarFile ./deploy.tar
+TAG=$TAG caprover deploy --appName $INPUT_APP --appToken $INPUT_TOKEN --caproverUrl $INPUT_SERVER --tarFile ./deploy.tar
